9c65d59acd7ce158c0e77a6877b5f90614b11471
theREALaltoid <oronazac@protonmail.com>
2019-10-27 14:53:06 -0700

    Updates

---
+++ precache-manifest.1509dd1c005124c5c4955174748dd7b0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f29bbf7942523d9b529c1417c1b91c",
+    "url": "/Plantr/index.html"
+  },
+  {
+    "revision": "1eab59b482aa41e62711",
+    "url": "/Plantr/static/css/2.fc7fbc74.chunk.css"
+  },
+  {
+    "revision": "25d148c2c606609a368b",
+    "url": "/Plantr/static/css/main.e1184a9e.chunk.css"
+  },
+  {
+    "revision": "1eab59b482aa41e62711",
+    "url": "/Plantr/static/js/2.e30d3e40.chunk.js"
+  },
+  {
+    "revision": "25d148c2c606609a368b",
+    "url": "/Plantr/static/js/main.85d20ea7.chunk.js"
+  },
+  {
+    "revision": "a0a5e92456b9bf4689f8",
+    "url": "/Plantr/static/js/runtime-main.1e7bb1f8.js"
+  },
+  {
+    "revision": "c65bc7fcdeb0c03d0bdf317ef52a1ae6",
+    "url": "/Plantr/static/media/aboutUs.c65bc7fc.jpg"
+  },
+  {
+    "revision": "056d84a746a7dc5dc18ee03ca722238d",
+    "url": "/Plantr/static/media/aboutustwo.056d84a7.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Plantr/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Plantr/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Plantr/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Plantr/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Plantr/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d53336e8df7b78f71d1e26fb916adf27",
+    "url": "/Plantr/static/media/homepage.d53336e8.jpg"
+  },
+  {
+    "revision": "c8a2fc0b836a09d87bea5dd49626fc0f",
+    "url": "/Plantr/static/media/screenshot.c8a2fc0b.png"
+  }
+]);
